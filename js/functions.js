cf9feb2470f8911a1859db5ffc855ebce2b33157
PabloRustrian <rustrianpablo@gmail.com>
2021-02-19 14:35:49 -0600

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,27 @@
-function GetYear() {
-    document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()));
+var XMLHttpRequestObject = false;
+var txtID = "txtID"
+var dataSource = "tc.txt"
+
+if (window.XMLHttpRequest) {
+  XMLHttpRequestObject = new XMLHttpRequest();
+} else if (window.ActiveXObject) {
+  XMLHttpRequestObject = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+function getData() {
+  if (XMLHttpRequestObject) {
+    var obj = document.getElementById(txtID);
+    XMLHttpRequestObject.open("GET", dataSource);
+
+    XMLHttpRequestObject.onreadystatechange = function () {
+      if (
+        XMLHttpRequestObject.readyState == 4 &&
+        XMLHttpRequestObject.status == 200
+      ) {
+        obj.innerHTML = XMLHttpRequestObject.responseText;
+      }
+    };
+
+    XMLHttpRequestObject.send(null);
   }
+}
